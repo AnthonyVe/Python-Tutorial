1d6cbd334faa249b79c6ffdfb226a34ee118b4ab
Anthony <>
2019-06-20 16:40:10 +0800

    Fixed function

---
+++ function2.py
@@ -0,0 +1,11 @@
+# Define our 3 functions
+def my_function():
+    print("Teams KD after match")
+
+def my_function_with_args(username, kdr, username2, kdr2, user3 ):
+    print("%s had a KD. of %s , %s had a KD of %s, %s peformed better"%(username, kdr, username2, kdr2, user3))
+#
+my_function()
+#
+my_function_with_args("Chris", "10.33", "John", "6.44", "Chris")
+#
