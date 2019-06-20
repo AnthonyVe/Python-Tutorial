e33e583353a91b4d6196a2156a126907404211d2
Anthony <>
2019-06-20 13:48:41 +0800

    Changes to lists and Basic operators added

---
+++ Basic-operators.py
@@ -0,0 +1,15 @@
+c = object()
+b = object()
+
+c_list = [b] * 10
+b_list = [b] * 10
+big_list = c_list + b_list * 2
+
+print("c_list contains %d objects" % len(b_list))
+print("b_list contains %d objects" % len(c_list))
+print("big_list contains %d objects" % len(big_list))
+
+if c_list.count(c) == 10 and b_list.count(b) == 10:
+    print("Almost there...")
+if big_list.count(c) == 10 and big_list.count(b) == 10:
+    print("Great!")
